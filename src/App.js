0dfccf6e62c78f1fcea25f7023aaddf41b236a99
Ajazsakib <saquibajaz999@gmail.com>
2024-01-04 00:03:05 +0530

    make json file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
 
 function App()
 {
+
+  const testApi = async () =>
+  {
+    await fetch("https://my-json-server.typicode.com/Ajazsakib/react-eCommerce", {
+      method: "POST",
+      body: JSON.stringify({
+        id: 1,
+        products: "Images"
+      })
+    })
+  }
+
+  useEffect(() =>
+  {
+    testApi()
+  }, [])
+
   return (
     <div className="App">
       <h1>React eCommerce App</h1>
